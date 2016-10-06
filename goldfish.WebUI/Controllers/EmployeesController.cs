9e2353ea0f643d724ae7b745897b6f535bc7c099
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-10-06 17:04:42 +0300

    0

---
+++ goldfish.WebUI/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using SX.WebCore;
+using SX.WebCore.MvcControllers;
+using System.Web.Mvc;
+
+namespace goldfish.WebUI.Controllers
+{
+    public sealed class EmployeesController : SxEmployeesController
+    {
+        public PartialViewResult ListForHome(int amount)
+        {
+            var filter = new SxFilter(1, amount);
+            var viewModel = Repo.Read(filter);
+
+            return PartialView("_ListForHome", viewModel);
+        }
+    }
+}
