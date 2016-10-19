1c3428e51806b3f08623beb2c2642e176b5411b0
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-10-19 17:15:15 +0300

    0

--- goldfish.WebUI/App_Start/RouteConfig.cs
+++ goldfish.WebUI/App_Start/RouteConfig.cs
@@ -1,15 +1,18 @@
-﻿using System.Web.Routing;
+﻿using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace goldfish.WebUI
 {
     public class RouteConfig
     {
-        public static void PreRouteAction(RouteCollection route)
-        {
+        private static readonly string[] _dafaultNamespaces = new string[] { "goldfish.WebUI.Controllers" };
 
+        public static void PreRouteAction(RouteCollection routes)
+        {
+            routes.MapRoute(name: null, url: "SiteServices/{titleUrl}", defaults: new { controller = "SiteServices", action = "Details"}, namespaces: _dafaultNamespaces);
         }
 
-        public static void PostRouteAction(RouteCollection route)
+        public static void PostRouteAction(RouteCollection routes)
         {
 
         }
---
+++ goldfish.WebUI/Areas/Admin/Controllers/SitePartnersController.cs
@@ -0,0 +1,10 @@
+﻿using SX.WebCore.DbModels;
+using SX.WebCore.MvcControllers;
+using SX.WebCore.ViewModels;
+
+namespace goldfish.WebUI.Areas.Admin.Controllers
+{
+    public sealed class SitePartnersController : SxSitePartnersController<SxSitePartner, SxVMSitePartner>
+    {
+    }
+}
