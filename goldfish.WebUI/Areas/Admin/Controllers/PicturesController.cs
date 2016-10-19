03e8192188c7966d3cc5c420d23b135de5d00bdc
simlex <simlex.dev.2014@gmail.com>
2016-10-20 00:58:10 +0300

    0

--- goldfish.WebUI/Areas/Admin/Controllers/PicturesController.cs
+++ goldfish.WebUI/Areas/Admin/Controllers/PicturesController.cs
@@ -1,9 +1,15 @@
-﻿using SX.WebCore.MvcControllers;
+﻿using goldfish.WebUI.Infrastructure.Repositories;
+using SX.WebCore.MvcControllers;
 
 namespace goldfish.WebUI.Areas.Admin.Controllers
 {
     public sealed class PicturesController : SxPicturesController
     {
-        
+        private static RepoPicture _repo = new RepoPicture();
+        public PicturesController():base()
+        {
+            if (Repo == null || (Repo as RepoPicture) == null)
+                Repo = _repo;
+        }
     }
 }
---
+++ goldfish.WebUI/Infrastructure/Repositories/RepoPicture.cs
@@ -0,0 +1,17 @@
+﻿using SX.WebCore.Repositories;
+using System;
+using System.Text;
+
+namespace goldfish.WebUI.Infrastructure.Repositories
+{
+    public sealed class RepoPicture : SxRepoPicture
+    {
+        protected override Action<StringBuilder> InsertNotFreePictures
+        {
+            get
+            {
+                return (sb)=> { };
+            }
+        }
+    }
+}
