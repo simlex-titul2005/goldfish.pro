27cbe9cfa111b8892acf413dcf564d9ba47ca498
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-11-03 09:07:41 +0200

    0

--- goldfish.WebUI/App_Start/RouteConfig.cs
+++ goldfish.WebUI/App_Start/RouteConfig.cs
@@ -11,12 +11,17 @@ namespace goldfish.WebUI
         {
             //employess
             routes.MapRoute(name: null, url: "Employees/{nikname}", defaults: new { controller = "Employees", action = "Details" }, namespaces: _dafaultNamespaces);
+            routes.MapRoute(name: null, url: "Employees", defaults: new { controller = "Employees", action = "List" }, namespaces: _dafaultNamespaces);
 
             //site projects
+            routes.MapRoute(name: null, url: "SiteProjects", defaults: new { controller = "SiteProjects", action = "List", page = 1 }, namespaces: _dafaultNamespaces);
+            routes.MapRoute(name: null, url: "SiteProjects/page{page}", defaults: new { controller = "SiteProjects", action = "List", page = 1 }, namespaces: _dafaultNamespaces);
             routes.MapRoute(name: null, url: "SiteProjects/{titleUrl}", defaults: new { controller = "SiteProjects", action = "Details" }, namespaces: _dafaultNamespaces);
 
             //site services
-            routes.MapRoute(name: null, url: "SiteServices/{titleUrl}", defaults: new { controller = "SiteServices", action = "Details"}, namespaces: _dafaultNamespaces);
+            routes.MapRoute(name: null, url: "SiteServices", defaults: new { controller = "SiteServices", action = "List", page = 1 }, namespaces: _dafaultNamespaces);
+            routes.MapRoute(name: null, url: "SiteServices/page{page}", defaults: new { controller = "SiteServices", action = "List", page = 1 }, namespaces: _dafaultNamespaces);
+            routes.MapRoute(name: null, url: "SiteServices/{titleUrl}", defaults: new { controller = "SiteServices", action = "Details" }, namespaces: _dafaultNamespaces);
         }
 
         public static void PostRouteAction(RouteCollection routes)
--- goldfish.WebUI/Controllers/MaterialsController.cs
+++ goldfish.WebUI/Controllers/MaterialsController.cs
@@ -1,4 +1,5 @@
-﻿using SX.WebCore.DbModels.Abstract;
+﻿using System.Web.Mvc;
+using SX.WebCore.DbModels.Abstract;
 using SX.WebCore.MvcControllers;
 using SX.WebCore.ViewModels;
 
@@ -9,5 +10,11 @@ namespace goldfish.WebUI.Controllers
         where TViewModel : SxVMMaterial, new()
     {
         public MaterialsController(byte mct) : base(mct) { }
+
+        public override ActionResult List(int page = 1, int pageSize = 10)
+        {
+            pageSize = 3;
+            return base.List(page, pageSize);
+        }
     }
 }
